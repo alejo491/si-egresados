2eca3f00f815a987c7a3760f8127eead88976bf7
ricardorado9@gmail.com <ricardorado9@gmail.com@1a176d30-b54c-86d7-6163-4fd3ccc015e2>
2013-04-08 01:33:22 +0000

    Agregados Controllers/CompaniesController.cs, Controllers/VacanciesController.cs y las Vistas para los CRUD de estas dos Clases

---
+++ trunk/AplicacionBase/Controllers/CompaniesController.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AplicacionBase.Models;
+
+namespace AplicacionBase.Controllers
+{
+    [Authorize]
+    public class CompaniesController : Controller
+    {
+        private DbSIEPISContext db = new DbSIEPISContext();
+
+        //
+        // GET: /Companies/
+
+        public ViewResult Index()
+        {
+            return View(db.Companies.ToList());
+        }
+
+        //
+        // GET: /Companies/Details/5
+
+        public ViewResult Details(Guid id)
+        {
+            Company company = db.Companies.Find(id);
+            return View(company);
+        }
+
+        //
+        // GET: /Companies/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        } 
+
+        //
+        // POST: /Companies/Create
+
+        [HttpPost]
+        public ActionResult Create(Company company)
+        {
+            if (ModelState.IsValid)
+            {
+                company.Id = Guid.NewGuid();
+                db.Companies.Add(company);
+                db.SaveChanges();
+                return RedirectToAction("Index");  
+            }
+
+            return View(company);
+        }
+        
+        //
+        // GET: /Companies/Edit/5
+ 
+        public ActionResult Edit(Guid id)
+        {
+            Company company = db.Companies.Find(id);
+            return View(company);
+        }
+
+        //
+        // POST: /Companies/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Company company)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(company).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(company);
+        }
+
+        //
+        // GET: /Companies/Delete/5
+ 
+        public ActionResult Delete(Guid id)
+        {
+            Company company = db.Companies.Find(id);
+            return View(company);
+        }
+
+        //
+        // POST: /Companies/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(Guid id)
+        {            
+            Company company = db.Companies.Find(id);
+            db.Companies.Remove(company);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
