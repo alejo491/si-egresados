f8c54787629a111359222cd58432cb4e563f5f2e
andresfeliperuizv@gmail.com <andresfeliperuizv@gmail.com@1a176d30-b54c-86d7-6163-4fd3ccc015e2>
2013-04-12 16:43:23 +0000

    Se arreglaron problemas en el index de Question y en el controlador de AnserChoices. Se corrige  Surveys/Index.cshtml adapatandolo a la nueva hoja de estilo. Se sube primer prototipo del controlador para llenar las encuestas.

---
+++ trunk/AplicacionBase/Models/ViewModels/AnswerViewModel.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace AplicacionBase.Models.ViewModels
+{
+    public class AnswerViewModel
+    {
+        
+        //public aspnet_Users User { get; private set; }
+        //public Dictionary<Question, String> Questions { get; set;}
+
+        //public AnswerViewModel(aspnet_Users user, IEnumerable<Question> questions)
+        //{
+        //    this.User = user;
+        //    foreach (var topic in questions)
+        //    {
+        //        this.Questions.Add(topic, "");
+        //    }
+        //}
+
+        public Dictionary<Question, String> Questions { get; set; }
+
+        public AnswerViewModel(IEnumerable<Question> questions)
+        {    
+            this.Questions = new Dictionary<Question, string>();
+            foreach (var topic in questions)
+            {
+                this.Questions.Add(topic, "");
+            }
+        }
+    }
+}
