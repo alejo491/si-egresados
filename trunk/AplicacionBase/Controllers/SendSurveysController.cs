a4eeb3d2bbf360ca1ff5e30ad6ffd927545c9606
davidpen8 <davidpen8@1a176d30-b54c-86d7-6163-4fd3ccc015e2>
2013-04-14 21:57:37 +0000

    Se crea, Controladores y vistas, para el modulo de enviar encuesta

---
+++ trunk/AplicacionBase/Controllers/SendSurveysController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AplicacionBase.Controllers
+{
+    public class SendSurveysController : Controller
+    {
+        //
+        // GET: /SendSurveys/
+
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+    }
+}
