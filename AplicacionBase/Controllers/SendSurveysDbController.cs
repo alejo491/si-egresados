67a3d1651d6dc26478bbd590ba01645a183113c7
davidpen8 <davidpen8@1a176d30-b54c-86d7-6163-4fd3ccc015e2>
2013-04-19 23:31:05 +0000

    Se Sube una versión, la cual ya puedes buscar, continua ahora lo visualizar antes de enviarlo (Jaime Jurado) También falta lo de enviar encuesta, por uno o mas emails especificos (Jhon Freddy)
    Debemos acordar con Jaime como recibe la informacion.

--- AplicacionBase/Controllers/SendSurveysDbController.cs
+++ AplicacionBase/Controllers/SendSurveysDbController.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using AplicacionBase.Models;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
@@ -8,7 +9,130 @@ namespace AplicacionBase.Controllers
 {
     public static class SendSurveysDbController
     {
-     
+        private static DbSIEPISContext db = new DbSIEPISContext();
+        
+        #region Obtener Email y Nombre de un Egresado por Programa y Fecha
+        /// <summary>
+        /// Consulta, que obtiene el email y nombre de los egresados de un programa, inferiores o iguales a la fecha ingresada
+        /// </summary>
+        /// <param name="programa">Programa al que pertenece un Egresado</param>
+        /// <param name="fecha">Fecha Maxima de Busqueda</param>
+        /// <returns>Dicitionary(email,nombre)</returns>
+        public static Dictionary<String, String> ListarEgresadosPrograma(string programa, DateTime fecha)
+        {
+            var listado = new Dictionary<string, string>();
+            var listadoEstudios = db.Studies.Where(s => s.Grade == programa).Where(s => s.EndDate <= fecha);
+            foreach (var listadoEstudio in listadoEstudios)
+            {
+                var idusuario = listadoEstudio.IdUser;
+                var usuario = db.aspnet_Membership.Find(idusuario);
+                var nombre = db.Users.Find(idusuario);
+                if (nombre != null)
+                {
+                    string nombreCompleto = nombre.FirstNames + " " + nombre.LastNames;
+                    listado.Add(usuario.Email, nombreCompleto);
+                }
+                else
+                {
+                    listado.Add(usuario.Email, "");
+                }
+            }
 
+            return listado;
+        }
+        #endregion
+
+        #region Obtener Email y Nombre de un Egresado por Programa
+        /// <summary>
+        /// Consulta, que obtiene el email y nombre de los egresados de un programa, inferiores o iguales a la fecha del sistema
+        /// </summary>
+        /// <param name="programa">Programa al que pertenece un egresado</param>
+        /// <returns>Dictionary(email,nombre)</returns>
+        public static Dictionary<String, String> ListarEgresadosPrograma(string programa)
+        {
+            var listado = new Dictionary<string, string>();
+            var listadoEstudios = db.Studies.Where(s => s.Grade == programa).Where(s => s.EndDate <= DateTime.Now);
+            foreach (var listadoEstudio in listadoEstudios)
+            {
+                var idusuario = listadoEstudio.IdUser;
+                var usuario = db.aspnet_Membership.Find(idusuario);
+                var nombre = db.Users.Find(idusuario);
+                if (nombre != null)
+                {
+                    string nombreCompleto = nombre.FirstNames + " " + nombre.LastNames;
+                    listado.Add(usuario.Email, nombreCompleto);
+                }
+                else
+                {
+                    listado.Add(usuario.Email, "");
+                }
+            }
+
+            return listado;
+        }
+        #endregion
+
+        #region Obtener Email y Nombre de un Jefe de Egresados por Programa y Fecha
+        /// <summary>
+        /// Consulta que obtiene un Jefe de un Egresado, Por Programa y Fecha Despues de Grado
+        /// </summary>
+        /// <param name="programa">Programa al que pertenecio un Egresado</param>
+        /// <param name="fecha">Fecha desde donde se desea buscar</param>
+        /// <returns>Dictionary(fecha, nombre)</returns>
+        public static Dictionary<String, String> ListarEgresadosJefe(string programa, DateTime fecha)
+        {
+            var listado = new Dictionary<string, string>();
+            var listadoEstudios = db.Studies.Where(s => s.Grade == programa).Where(s => s.EndDate >= fecha);
+            foreach (var listadoEstudio in listadoEstudios)
+            {
+                var aux = listadoEstudio;
+                var experiencia = db.Experiences.Where(e => e.IdUser == aux.IdUser);
+                foreach (var experience in experiencia)
+                {
+                    var exp = experience;
+                    var expBoss = db.ExperiencesBosses.Where(ex => ex.IdExperiences == exp.Id);
+                    foreach (var experiencesBoss in expBoss)
+                    {
+                        var boss = db.Bosses.Find(experiencesBoss.IdBoss);
+                        listado.Add(boss.Email, boss.Name);
+                    }
+                }
+                
+            }
+
+            return listado;
+        }
+        #endregion
+        
+        #region Obtener Email y Nombre de un Jefe de Egresado Por Programa
+        /// <summary>
+        /// Consulta, que obtiene el Email y Nombre de un Egresado de un Programa que tenga Jefe
+        /// </summary>
+        /// <param name="programa">Programa al que pertenecion el Egresado</param>
+        /// <returns>Dictionary(email,nombre)</returns>
+        public static Dictionary<String, String> ListarEgresadosJefe(string programa)
+        {
+            var listado = new Dictionary<string, string>();
+            var listadoEstudios = db.Studies.Where(s => s.Grade == programa).Where(s => s.EndDate >= DateTime.Now);
+            foreach (var listadoEstudio in listadoEstudios)
+            {
+                var aux = listadoEstudio;
+                var experiencia = db.Experiences.Where(e => e.IdUser == aux.IdUser);
+                foreach (var experience in experiencia)
+                {
+                    var exp = experience;
+                    var expBoss = db.ExperiencesBosses.Where(ex => ex.IdExperiences == exp.Id);
+                    foreach (var experiencesBoss in expBoss)
+                    {
+                        var boss = db.Bosses.Find(experiencesBoss.IdBoss);
+                        listado.Add(boss.Email, boss.Name);
+                    }
+                }
+
+            }
+
+            return listado;
+        }
+        #endregion
     }
 }
