23b67537ce3b9fb79b7cdc5687423c28959ee780
wilcamvel22@gmail.com <wilcamvel22@gmail.com@1a176d30-b54c-86d7-6163-4fd3ccc015e2>
2013-04-21 19:35:25 +0000

    Creacion del crud de Roles y Estudios

---
+++ trunk/AplicacionBase/Controllers/RolesController.cs
@@ -0,0 +1,119 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AplicacionBase.Models;
+
+namespace AplicacionBase.Controllers
+{ 
+    public class RolesController : Controller
+    {
+        private DbSIEPISContext db = new DbSIEPISContext();
+
+        //
+        // GET: /Roles/
+
+        public ViewResult Index()
+        {
+            var aspnet_roles = db.aspnet_Roles.Include(a => a.aspnet_Applications);
+            return View(aspnet_roles.ToList());
+        }
+
+        //
+        // GET: /Roles/Details/5
+
+        public ViewResult Details(Guid id)
+        {
+            aspnet_Roles aspnet_roles = db.aspnet_Roles.Find(id);
+            return View(aspnet_roles);
+        }
+
+        //
+        // GET: /Roles/Create
+
+        public ActionResult Create()
+        {
+            //ViewBag.ApplicationId = new SelectList(db.aspnet_Applications, "ApplicationId", "ApplicationName");
+            //ViewBag.Id = new SelectList(db.aspnet_Users, "RolesId", "UserName");
+            return View();
+        } 
+
+        //
+        // POST: /Roles/Create
+
+        [HttpPost]
+        public ActionResult Create(aspnet_Roles aspnet_roles)
+        {
+            if (ModelState.IsValid)
+            {
+                var IdApplication = db.aspnet_Applications.First().ApplicationId;
+                aspnet_roles.ApplicationId = IdApplication;
+                var minuscula = aspnet_roles.RoleName;
+                aspnet_roles.LoweredRoleName = minuscula.ToLower();
+                aspnet_roles.RoleId = Guid.NewGuid();
+                db.aspnet_Roles.Add(aspnet_roles);
+                db.SaveChanges();
+                return RedirectToAction("Index");  
+            }
+
+            ViewBag.ApplicationId = new SelectList(db.aspnet_Applications, "ApplicationId", "ApplicationName", aspnet_roles.ApplicationId);
+            return View(aspnet_roles);
+        }
+        
+        //
+        // GET: /Roles/Edit/5
+ 
+        public ActionResult Edit(Guid id)
+        {
+            aspnet_Roles aspnet_roles = db.aspnet_Roles.Find(id);
+            ViewBag.ApplicationId = new SelectList(db.aspnet_Applications, "ApplicationId", "ApplicationName", aspnet_roles.ApplicationId);
+            return View(aspnet_roles);
+        }
+
+        //
+        // POST: /Roles/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(aspnet_Roles aspnet_roles)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(aspnet_roles).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.ApplicationId = new SelectList(db.aspnet_Applications, "ApplicationId", "ApplicationName", aspnet_roles.ApplicationId);
+            return View(aspnet_roles);
+        }
+
+        //
+        // GET: /Roles/Delete/5
+ 
+        public ActionResult Delete(Guid id)
+        {
+            aspnet_Roles aspnet_roles = db.aspnet_Roles.Find(id);
+            return View(aspnet_roles);
+        }
+
+        //
+        // POST: /Roles/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(Guid id)
+        {            
+            aspnet_Roles aspnet_roles = db.aspnet_Roles.Find(id);
+            db.aspnet_Roles.Remove(aspnet_roles);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ trunk/AplicacionBase/Controllers/StudyController.cs
@@ -0,0 +1,122 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AplicacionBase.Models;
+
+namespace AplicacionBase.Controllers
+{ 
+    public class StudyController : Controller
+    {
+        private DbSIEPISContext db = new DbSIEPISContext();
+
+        //
+        // GET: /Study/
+
+        public ViewResult Index()
+        {
+            var studies = db.Studies.Include(s => s.School).Include(s => s.User).Include(s => s.Thesis);
+            return View(studies.ToList());
+        }
+
+        //
+        // GET: /Study/Details/5
+
+        public ViewResult Details(Guid id)
+        {
+            Study study = db.Studies.Find(id);
+            return View(study);
+        }
+
+        //
+        // GET: /Study/Create
+
+        public ActionResult Create()
+        {
+            ViewBag.IdSchool = new SelectList(db.Schools, "Id", "Name");
+            ViewBag.IdUser = new SelectList(db.Users, "Id", "PhoneNumber");
+            ViewBag.Id = new SelectList(db.Theses, "IdStudies", "Title");
+            return View();
+        } 
+
+        //
+        // POST: /Study/Create
+
+        [HttpPost]
+        public ActionResult Create(Study study)
+        {
+            if (ModelState.IsValid)
+            {
+                study.Id = Guid.NewGuid();
+                db.Studies.Add(study);
+                db.SaveChanges();
+                return RedirectToAction("Index");  
+            }
+
+            ViewBag.IdSchool = new SelectList(db.Schools, "Id", "Name", study.IdSchool);
+            ViewBag.IdUser = new SelectList(db.Users, "Id", "PhoneNumber", study.IdUser);
+            ViewBag.Id = new SelectList(db.Theses, "IdStudies", "Title", study.Id);
+            return View(study);
+        }
+        
+        //
+        // GET: /Study/Edit/5
+ 
+        public ActionResult Edit(Guid id)
+        {
+            Study study = db.Studies.Find(id);
+            ViewBag.IdSchool = new SelectList(db.Schools, "Id", "Name", study.IdSchool);
+            ViewBag.IdUser = new SelectList(db.Users, "Id", "PhoneNumber", study.IdUser);
+            ViewBag.Id = new SelectList(db.Theses, "IdStudies", "Title", study.Id);
+            return View(study);
+        }
+
+        //
+        // POST: /Study/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Study study)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(study).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.IdSchool = new SelectList(db.Schools, "Id", "Name", study.IdSchool);
+            ViewBag.IdUser = new SelectList(db.Users, "Id", "PhoneNumber", study.IdUser);
+            ViewBag.Id = new SelectList(db.Theses, "IdStudies", "Title", study.Id);
+            return View(study);
+        }
+
+        //
+        // GET: /Study/Delete/5
+ 
+        public ActionResult Delete(Guid id)
+        {
+            Study study = db.Studies.Find(id);
+            return View(study);
+        }
+
+        //
+        // POST: /Study/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(Guid id)
+        {            
+            Study study = db.Studies.Find(id);
+            db.Studies.Remove(study);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
