2abf21e5d111229f1b44613b4525c7f32945cbbb
davidpen8 <davidpen8@1a176d30-b54c-86d7-6163-4fd3ccc015e2>
2013-04-24 08:22:05 +0000

    Se agregan, validaciones de los campos. Las Consultas a la Base de Datos, estan presentando problemas (Revisar)

--- AplicacionBase/Controllers/SendSurveysDbController.cs
+++ AplicacionBase/Controllers/SendSurveysDbController.cs
@@ -42,6 +42,38 @@ namespace AplicacionBase.Controllers
         }
         #endregion
 
+        #region Obtener Email y Nombre de un Egresado por Programa y Entre Fechas
+        /// <summary>
+        /// Consulta, que obtiene el email y nombre de los egresados de un programa, Entre 2 fechas ingresadas
+        /// </summary>
+        /// <param name="programa">Programa al que pertenece un Egresado</param>
+        /// <param name="fecha">Fecha Maxima de Busqueda</param>
+        /// <returns>Dicitionary(email,nombre)</returns>
+        public static Dictionary<String, String> ListarEgresadosPrograma(string programa, DateTime fechaDesde, DateTime fechaHasta)
+        {
+            var listado = new Dictionary<string, string>();
+            var listadoEstudios =
+                db.Studies.Where(s => s.Grade == programa).Where(s => s.EndDate >= fechaDesde && s.EndDate < fechaHasta);
+            foreach (var listadoEstudio in listadoEstudios)
+            {
+                var idusuario = listadoEstudio.IdUser;
+                var usuario = db.aspnet_Membership.Find(idusuario);
+                var nombre = db.Users.Find(idusuario);
+                if (nombre != null)
+                {
+                    string nombreCompleto = nombre.FirstNames + " " + nombre.LastNames;
+                    listado.Add(usuario.Email, nombreCompleto);
+                }
+                else
+                {
+                    listado.Add(usuario.Email, "");
+                }
+            }
+
+            return listado;
+        }
+        #endregion
+
         #region Obtener Email y Nombre de un Egresado por Programa
         /// <summary>
         /// Consulta, que obtiene el email y nombre de los egresados de un programa, inferiores o iguales a la fecha del sistema
@@ -100,6 +132,38 @@ namespace AplicacionBase.Controllers
                 
             }
 
+            return listado;
+        }
+        #endregion
+
+        #region Obtener Email y Nombre de un Jefe de Egresados por Programa y Entre Fechas
+        /// <summary>
+        /// Consulta que obtiene un Jefe de un Egresado, Por Programa y Especificando una Fecha entre los Egresados
+        /// </summary>
+        /// <param name="programa">Programa al que pertenecio un Egresado</param>
+        /// <param name="fechaDesde">Fecha Desde donde se desea buscar</param>
+        /// <param name="fechaHasta">Fecha Hasta donde se desea buscar</param>
+        /// <returns>Dictionary(fecha, nombre)</returns>
+        public static Dictionary<String, String> ListarEgresadosJefe(string programa, DateTime fechaDesde, DateTime fechaHasta)
+        {
+            var listado = new Dictionary<string, string>();
+            var listadoEstudios = db.Studies.Where(s => s.Grade == programa).Where(s => s.EndDate >= fechaDesde && s.EndDate<=fechaHasta);
+            foreach (var listadoEstudio in listadoEstudios)
+            {
+                var aux = listadoEstudio;
+                var experiencia = db.Experiences.Where(e => e.IdUser == aux.IdUser);
+                foreach (var experience in experiencia)
+                {
+                    var exp = experience;
+                    var expBoss = db.ExperiencesBosses.Where(ex => ex.IdExperiences == exp.Id);
+                    foreach (var experiencesBoss in expBoss)
+                    {
+                        var boss = db.Bosses.Find(experiencesBoss.IdBoss);
+                        listado.Add(boss.Email, boss.Name);
+                    }
+                }
+
+            }
             return listado;
         }
         #endregion
