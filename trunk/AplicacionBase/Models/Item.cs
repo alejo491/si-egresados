26fb5b3027c2fa392eb2ee55eca15cf3d0dfa7fd
amorenofx@gmail.com <amorenofx@gmail.com@1a176d30-b54c-86d7-6163-4fd3ccc015e2>
2013-05-05 22:11:41 +0000

    Se adaptaron las vistas del módulo (agrego menú) y se realizarón los cambios necesarios del CRUD reportes.

--- trunk/AplicacionBase/Models/Item.cs
+++ trunk/AplicacionBase/Models/Item.cs
@@ -1,5 +1,7 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Web.Mvc;
 
 namespace AplicacionBase.Models
 {
@@ -7,9 +9,18 @@ namespace AplicacionBase.Models
     {
         public System.Guid Id { get; set; }
         public System.Guid IdReport { get; set; }
+
+        [Required(ErrorMessage = " ¡El campo es obligatorio!")]
         public string TableName { get; set; }
+
+        [Required(ErrorMessage = " ¡El campo es obligatorio!")]
         public string FieldName { get; set; }
+
+        [Required(ErrorMessage = " ¡El campo es obligatorio!")]
+        [DisplayFormat(ApplyFormatInEditMode = true, DataFormatString = "{0:g}")]
         public decimal PageNumber { get; set; }
+
+        [Required(ErrorMessage = " ¡El campo es obligatorio!")]
         public virtual Report Report { get; set; }
     }
 }
