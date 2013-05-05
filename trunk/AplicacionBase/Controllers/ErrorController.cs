2df39d71fe905c8097d181be2b33a0f53320a665
andresfeliperuizv@gmail.com <andresfeliperuizv@gmail.com@1a176d30-b54c-86d7-6163-4fd3ccc015e2>
2013-05-05 18:25:34 +0000

    Se modifica el web.config para el manejo de violaciones de seguridad y acceso. Se añaden controladores para el manejo de errores y excepciones.

---
+++ trunk/AplicacionBase/Controllers/ErrorController.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace AplicacionBase.Controllers
+{
+    public class ErrorController : Controller
+    {
+        //
+        // GET: /Error/
+
+        public ViewResult Index(string aspxerrorpath)
+        {
+            
+            if (aspxerrorpath != null)
+            {
+                ViewBag.ErrorType = "La pagina "+aspxerrorpath+" no existe";
+            }
+            return View();
+        }
+
+    }
+}
