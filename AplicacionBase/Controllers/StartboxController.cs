366bf9781df7fd9d78cbb1fac9743bfbdd7991d1
luisordoniez@gmail.com <luisordoniez@gmail.com@1a176d30-b54c-86d7-6163-4fd3ccc015e2>
2013-05-25 04:59:37 +0000

    controladores del megusta y la calificacion

---
+++ AplicacionBase/Controllers/StartboxController.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using AplicacionBase.Models;
+
+namespace AplicacionBase.Controllers
+{ 
+    public class StartboxController : Controller
+    {
+        private DbSIEPISContext db = new DbSIEPISContext();
+
+        //
+        // GET: /Startbox/
+
+        public ViewResult Index()
+        {
+            return View(db.Startboxs.ToList());
+        }
+
+        //
+        // GET: /Startbox/Details/5
+
+        public ViewResult Details(Guid id)
+        {
+            Startbox startbox = db.Startboxs.Find(id);
+            return View(startbox);
+        }
+
+        //
+        // GET: /Startbox/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        } 
+
+        //
+        // POST: /Startbox/Create
+
+        [HttpPost]
+        public ActionResult Create(Startbox startbox)
+        {
+            if (ModelState.IsValid)
+            {
+                startbox.Id = Guid.NewGuid();
+                db.Startboxs.Add(startbox);
+                db.SaveChanges();
+                return RedirectToAction("Index");  
+            }
+
+            return View(startbox);
+        }
+        
+        //
+        // GET: /Startbox/Edit/5
+ 
+        public ActionResult Edit(Guid id)
+        {
+            Startbox startbox = db.Startboxs.Find(id);
+            return View(startbox);
+        }
+
+        //
+        // POST: /Startbox/Edit/5
+
+        [HttpPost]
+        public ActionResult Edit(Startbox startbox)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(startbox).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(startbox);
+        }
+
+        //
+        // GET: /Startbox/Delete/5
+ 
+        public ActionResult Delete(Guid id)
+        {
+            Startbox startbox = db.Startboxs.Find(id);
+            return View(startbox);
+        }
+
+        //
+        // POST: /Startbox/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        public ActionResult DeleteConfirmed(Guid id)
+        {            
+            Startbox startbox = db.Startboxs.Find(id);
+            db.Startboxs.Remove(startbox);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
