64dd06c983f07c23384b666c5fac876bbda40006
davidpen8 <davidpen8@1a176d30-b54c-86d7-6163-4fd3ccc015e2>
2013-06-12 23:56:59 +0000

    Se Agregan Comentarios al Controlador Questions y Se eliminan comentarios de codigo, buscando dejar mejor organizado el codigo.

--- trunk/AplicacionBase/Controllers/QuestionsController.cs
+++ trunk/AplicacionBase/Controllers/QuestionsController.cs
@@ -1,34 +1,29 @@
 ﻿using System;
-using System.Collections.Generic;
 using System.Data;
 using System.Data.Entity;
 using System.Linq;
-using System.Web;
 using System.Web.Mvc;
 using AplicacionBase.Models;
 
 namespace AplicacionBase.Controllers
 { 
+    /*Controlador Finalizado, Documentado*/
+
     public class QuestionsController : Controller
     {
         private DbSIEPISContext db = new DbSIEPISContext();
 
-        //
-        // GET: /Questions/
-        /*
-        public ViewResult Index()
-        {
-          //  var questions = db.Questions.Include(q => q.Topic);
-            return View(db.Questions.ToList());
-        }
-        */
+        /// <summary>
+        /// Esta funcion se llama para mostrar las Preguntas (Questions) asociadas a un Tema (Topic)
+        /// </summary>
+        /// <param name="id"> GUID (Identificador) del Tema</param>
+        /// <returns>Listado de Preguntas Asociadas a un Tema</returns>
         public ActionResult Index(Guid? id)
         {
-           
             if (id != Guid.Empty && id != null)
             {
 
-                var topic = (Topic) db.Topics.Find(id);
+                var topic = db.Topics.Find(id);
 
                 if (topic != null)
                 {
@@ -36,67 +31,58 @@ namespace AplicacionBase.Controllers
                     ViewBag.Topic = topic;
                     return View(questionsTopics.ToList());
                 }
-                else
-                {
-                    return RedirectToAction("Index", "Home");
-                }
-            }
-            else
-            {
                 return RedirectToAction("Index", "Home");
             }
+            return RedirectToAction("Index", "Home");
         }
 
-        //
-        // GET: /Questions/Details/5
-        
-
+        /// <summary>
+        /// Vista que muestra La información, que pertenece a una Pregunta
+        /// </summary>
+        /// <param name="id">GUID del la pregunta</param>
+        /// <returns>Una (ViewResult) Vista que contiene la información de la Pregunta </returns>
 
         public ViewResult Details(Guid id)
         {
             Question question = db.Questions.Find(id);
             var auxTopic = db.Topics.Find(question.IdTopic);
-            //ViewBag.Questions = question;
             ViewBag.Topic = auxTopic;
             return View(question);
         }
 
-        //
-        // GET: /Questions/Create
+        /// <summary>
+        /// Vista que Asocia una Pregunta a un Tema 
+        /// </summary>
+        /// <param name="id">Identificador del Tema</param>
+        /// <returns>ActionResult de la Vista y un Formulario para asociar, la Pregunta al Tema</returns>
 
         public ActionResult Create(Guid? id)
         {
-
             if (id != Guid.Empty && id != null)
             {
-                var auxTopic = (Topic)db.Topics.Find(id);
+                var auxTopic = db.Topics.Find(id);
 
                 if (auxTopic != null)
                 {
                     ViewBag.Topic = auxTopic;
                     return View();
                 }
-                else
-                {
-                    return RedirectToAction("Index", "Home");
-                }
-            }
-            else
-            {
                 return RedirectToAction("Index", "Home");
             }
+            return RedirectToAction("Index", "Home");
         }
 
-        /*
-        public ActionResult Create()
-        {
-            ViewBag.IdTopic = new SelectList(db.Topics, "Id", "Description");
-            return View();
-        } 
-        */
         //
         // POST: /Questions/Create
 
+        /// <summary>
+        /// Se llama luego que el usuario oprime el boton Crear Pregunta, si ocurre un error redirecciona al usuario a 
+        /// una pagina de error.
+        /// </summary>
+        /// <param name="id">GUID (Identificador del Tema)</param>
+        /// <param name="question">Objeto que se crea en el formulario</param>
+        /// <returns>ActionResult Redirecciona al listado de Preguntas de un Tema Especifico</returns>
+        
         [HttpPost]
         public ActionResult Create(Guid? id, Question question)
         {
@@ -105,7 +91,7 @@ namespace AplicacionBase.Controllers
             {
                 question.Id = Guid.NewGuid();
                 question.IdTopic = new Guid("" + id);
-                var topic = (Topic)db.Topics.Find(id); 
+                var topic = db.Topics.Find(id); 
                 bool existeNumero = ExisteNumero(question.QuestionNumber, question.IdTopic, question.Id);
 
                 if (existeNumero)
@@ -130,41 +116,24 @@ namespace AplicacionBase.Controllers
                   
                     return RedirectToAction("Index", new {id = id});
                }
-                else
+                if (topic != null)
                 {
-                    if (topic != null)
-                    {
-                        ViewBag.Topic = topic;
-                    }
-                    TempData["Error2"] = "Este número ya ha sido asignado a otra pregunta";
-                    return View();
+                    ViewBag.Topic = topic;
                 }
-                
+                TempData["Error2"] = "Este número ya ha sido asignado a otra pregunta";
+                return View();
             }
 
            // ViewBag.IdTopic = new SelectList(db.Topics, "Id", "Description", question.IdTopic);
             return View(question);
         }
-
-        /*
-        [HttpPost]
-        public ActionResult Create(Question question)
-        {
-            if (ModelState.IsValid)
-            {
-                question.Id = Guid.NewGuid();
-                db.Questions.Add(question);
-                db.SaveChanges();
-                return RedirectToAction("Index");  
-            }
-
-            ViewBag.IdTopic = new SelectList(db.Topics, "Id", "Description", question.IdTopic);
-            return View(question);
-        }
-        */
-        //
-        // GET: /Questions/Edit/5
  
+        /// <summary>
+        /// Se llama cuando el usuario en el listado presiona editar sobre una Pregunta
+        /// </summary>
+        /// <param name="idT">GUID (Identificador) del Tema</param>
+        /// <param name="idQ">GUID (Identificador) de la Pregunta</param>
+        /// <returns>Retorna una vista con la informacion editable de la Pregunta</returns>
 
         public ActionResult Edit(Guid? idT, Guid? idQ)
         {
@@ -176,20 +145,24 @@ namespace AplicacionBase.Controllers
 
                 if (question != null || auxTopic != null)
                 {
-                    //ViewBag.IdTopic = new SelectList(db.Topics, "Id", "Description", question.IdTopic);
                     ViewBag.Topic = auxTopic;
                     return View(question);
                 }
-                else
-                {
-                    return RedirectToAction("Index", "Home");
-                }
+                return RedirectToAction("Index", "Home");
             }
             return RedirectToAction("Index", "Home");
         }
 
         //
-        // POST: /Questions/Edit/5
+        // POST: /Questions/Edit/
+
+        /// <summary>
+        /// Se llama despues de que el usuario oprime el boton Guardar
+        /// </summary>
+        /// <param name="idT">GUID (Identificador) del Tema</param>
+        /// <param name="idQ">GUID (Identificador) de la Pregunta</param>
+        /// <param name="question">Objeto Modificado de la Pregunta</param>
+        /// <returns>Redirecciona al listado de Preguntas, luego de que el objeto se modifique correctamente</returns>
 
         [HttpPost]
         public ActionResult Edit(Guid idT, Guid idQ, Question question)
@@ -231,23 +204,12 @@ namespace AplicacionBase.Controllers
         }
 
 
-        /*
-        [HttpPost]
-        public ActionResult Edit(Question question)
-        {
-            if (ModelState.IsValid)
-            {
-                db.Entry(question).State = EntityState.Modified;
-                db.SaveChanges();
-                return RedirectToAction("Index");
-            }
-            ViewBag.IdTopic = new SelectList(db.Topics, "Id", "Description", question.IdTopic);
-            return View(question);
-        }
-        */
-        //
-        // GET: /Questions/Delete/5
- 
+        /// <summary>
+        /// Elimina la Pregunta de un Tema Especifico
+        /// </summary>
+        /// <param name="id">GUID (Identificador) de la Pregunta</param>
+        /// <returns>Redirecciona a la Vista de Confirmar Operacion</returns>
+        
         public ActionResult Delete(Guid id)
         {
            
@@ -262,7 +224,13 @@ namespace AplicacionBase.Controllers
         }
 
         //
-        // POST: /Questions/Delete/5
+        // POST: /Questions/Delete/
+
+        /// <summary>
+        /// Se llama después de que el Usuario confirme eliminar la Pregunta
+        /// </summary>
+        /// <param name="id">GUID (Identificador) de la Pregunta</param>
+        /// <returns>Redirecciona al listado de Preguntas, después de borrar la Pregunta</returns>
 
         [HttpPost, ActionName("Delete")]
         public ActionResult DeleteConfirmed(Guid id)
@@ -276,49 +244,38 @@ namespace AplicacionBase.Controllers
             return RedirectToAction("Index", new {id=auxTopic.Id});
         }
 
+        /// <summary>
+        /// Método que se ejecuta antes de cargar una vista
+        /// </summary>
+        /// <param name="disposing">Recibe si destruye o no un Objeto</param>
+
         protected override void Dispose(bool disposing)
         {
             db.Dispose();
             base.Dispose(disposing);
         }
-
-
-        /// Metodo que retorna una validacion si el número de pregunta que se le va asignar a la Pregunta es único
+ 
+        /// <summary>
+        /// Metodo que retorna una validacion si el número de pregunta que se le va asignar a la Pregunta es único 
         /// </summary>
         /// <param name="questionNumber">Numero de la Pregunta</param>
-        /// <param name="IdTopic">Id del Tema</param>
-        /// <param name="IdQuestion">Id de la Pregunta</param>
-        /// <returns>Verdadero si el numero no existe, falso si el numero ya existe</returns>
-
-        public bool ExisteNumero(Decimal questionNumber, Guid idTopic, Guid IdQuestion)
+        /// <param name="idTopic">GUID (Identificador) del  Tema</param>
+        /// <param name="idQuestion">GUID (Identificador) de la Pregunta</param>
+        /// <returns>Verdadero (True) si el numero no existe, Falso (False) si el numero ya existe</returns>
+        
+        public bool ExisteNumero(Decimal questionNumber, Guid idTopic, Guid idQuestion)
         {
             //var question = db.Questions.Where(a => a.Id == IdAnswer).
             var q = db.Questions.Where(s => s.IdTopic == idTopic);
 
             foreach (Question question in q)
             {
-                if (question.QuestionNumber == questionNumber && question.Id != IdQuestion)
+                if (question.QuestionNumber == questionNumber && question.Id != idQuestion)
                 {
                     return false;
                 }
             }
             return true;
         }
-
-        /*
-        [HttpPost]
-        public JsonResult ExisteNumero(Decimal QuestionNumber)
-        {
-            var questions = db.Questions;
-            foreach (Question q in questions)
-            {
-                if (q.QuestionNumber == QuestionNumber)
-                {
-                    return Json(false);
-                }
-            }
-            return Json(true);
-        }
-         */
     }
 }
