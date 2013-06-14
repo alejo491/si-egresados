3a31ba9d614c7818a7d729ff24e40deb29f1cd45
andresfeliperuizv@gmail.com <andresfeliperuizv@gmail.com@1a176d30-b54c-86d7-6163-4fd3ccc015e2>
2013-06-14 23:47:39 +0000

    Se crea un menu desplegable para las opciones de los listados.

---
+++ trunk/AplicacionBase/Scripts/menu.js
@@ -0,0 +1,24 @@
+﻿$.fn.extend({
+    botonMenu: function () {
+        this.each(function () {
+            $(this).on("click", mensaje);
+        });
+    }
+});
+
+function mensaje() {
+    $(this).children(0).toggle("fast");
+    if ($(this).hasClass("seleccionado")) {
+        $(this).removeClass("seleccionado");
+    } else {
+        $(this).addClass("seleccionado");
+    }
+}
+
+
+$(document).on("ready", main);
+
+function main() {
+    $(".boton").botonMenu();
+    
+}
