a450bf71e23e20914aeb6547d636a0e1c7d37764
christhian88@gmail.com <christhian88@gmail.com@1a176d30-b54c-86d7-6163-4fd3ccc015e2>
2013-06-15 16:03:50 +0000

    Se agrego el buscador y se hacen comentarios en los codigos de los controladores: UserController y VerifyController.

--- trunk/AplicacionBase/Controllers/UserController.cs
+++ trunk/AplicacionBase/Controllers/UserController.cs
@@ -8,16 +8,40 @@ using System.Web.Mvc;
 using System.Web.Routing;
 using System.Web.Security;
 using AplicacionBase.Models;
+using PagedList;
 
 namespace AplicacionBase.Controllers
 {
+    /// <summary>
+    /// Controlador para gestionar los usuarios que estan registrados en el sistema
+    /// </summary>
     public class UserController : Controller
     {
+        /// <summary>
+        /// Atributo que consulta la base de datos.
+        /// </summary>
         private DbSIEPISContext db = new DbSIEPISContext();
+        /// <summary>
+        /// Atributos que permite controlar la paginación del buscador.
+        /// </summary>
+        private int pageSize = 1;
+        private int pageNumber;
+        /// <summary>
+        /// Atributo que recorre la lista de usuarios.
+        /// </summary>
+        private System.Linq.IOrderedEnumerable<User> results;
+        /// <summary>
+        /// Atributos que perm.
+        /// </summary>
+        private string searchText;
 
         //
         // GET: /User/
 
+        /// <summary>
+        /// Método que carga la vista que contiene todos los usuarios registrados en el sistema
+        /// </summary>
+        /// <returns>Vista que contine todos los usuarios</returns>
         public ViewResult Index()
         {
             var users = db.Users.Include(u => u.aspnet_Users);
@@ -25,23 +49,25 @@ namespace AplicacionBase.Controllers
             return View(users.ToList());
         }
 
+        /// <summary>
+        /// Método que carga la vista principal de un usuario
+        /// </summary>
+        /// <param name="id">Id del usuario que ingresa al sistema</param>
+        /// <returns>Vista principal de un usuario con todas sus opciones</returns>
         public ActionResult Begin(Guid id)
         {
             User user = db.Users.Find(id);
-            if (user == null)
-            {
-                return View();
-            }
-            else
-            {
-                ViewBag.Id = new SelectList(db.aspnet_Users, "UserId", "UserName", user.Id);
-                return View(user);
-            }
+            ViewBag.Id = new SelectList(db.aspnet_Users, "UserId", "UserName", user.Id);
+            return View(user);
         }
 
         //
         // GET: /User/Details/5
-
+        /// <summary>
+        /// Método que carga la vista que contine los datos de un usuario
+        /// </summary>
+        /// <param name="id">Id del usuario</param>
+        /// <returns>Vista para consultar los datos de un usuario</returns>
         public ViewResult Details(Guid id)
         {
             User user = db.Users.Find(id);
@@ -50,7 +76,10 @@ namespace AplicacionBase.Controllers
 
         //
         // GET: /User/Create
-
+        /// <summary>
+        /// Método que carga la vista con el formulario para crear la información personal de un usuario
+        /// </summary>
+        /// <returns>Vista que despliega el formulario que permite crear los datos</returns>
         public ActionResult Create()
         {
             ViewBag.UserName = HttpContext.User.Identity.Name;
@@ -58,8 +87,10 @@ namespace AplicacionBase.Controllers
             return View();
         }
 
-        //
-        // POST: /User/Create
+        /// <summary>
+        /// Método que obtiene el Id del usuario que esta logueado en el sistema
+        /// </summary>
+        /// <returns>Id del usuario</returns>
         public Guid searchId()
         {
             Guid g = System.Guid.Empty;
@@ -73,13 +104,19 @@ namespace AplicacionBase.Controllers
             return g;
         }
 
+        //
+        // POST: /User/Create
+        /// <summary>
+        /// Guarda los datos del usuario recibidos en el formulario
+        /// </summary>
+        /// <param name="user">Usuario con toda su información</param>
+        /// <returns></returns>
         [HttpPost]
         public ActionResult Create(User user)
         {
             Guid g = searchId();
             if (ModelState.IsValid && !g.Equals(System.Guid.Empty))
             {
-                //string nombre = HttpContext.cu .User.Identity
                 user.Id = g;
                 user.States = "true";
                 db.Users.Add(user);
@@ -97,7 +134,6 @@ namespace AplicacionBase.Controllers
                     var ActualStep = Convert.ToInt16(tmp.ElementAt(0).Step.SOrder);
                     return RedirectToAction("Index", "Wizard", ActualStep);
                 }
-                //return RedirectToAction("Index", "Home");
             }
             ViewBag.Id = new SelectList(db.aspnet_Users, "UserId", "UserName", user.Id);
             return View(user);
@@ -123,18 +159,25 @@ namespace AplicacionBase.Controllers
 
         //
         // GET: /User/Edit/5
-
+        /// <summary>
+        /// Método que carga la vista con el formulario para editar la información personal de un usuario
+        /// </summary>
+        /// <param name="id">Id del usuario</param>
+        /// <returns>Vista que despliega el formulario con los datos para editarlos</returns>
         public ActionResult Edit(Guid id)
         {
             User user = db.Users.Find(id);
             ViewBag.Id = new SelectList(db.aspnet_Users, "UserId", "UserName", user.Id);
-            //return RedirectToAction("Index", "User");
             return View(user);
         }
 
         //
         // POST: /User/Edit/5
-
+        /// <summary>
+        /// Guarda los cambios de la información del usuario recibidos en el formulario
+        /// </summary>
+        /// <param name="user">Usuario con toda su información</param>
+        /// <returns></returns>
         [HttpPost]
         public ActionResult Edit(User user)
         {
@@ -158,7 +201,11 @@ namespace AplicacionBase.Controllers
             return View(user);
         }
 
-
+        /// <summary>
+        /// Método que carga la vista que permite modificar el estado de un usuario en el sistema
+        /// </summary>
+        /// <param name="id">Id del usuario</param>
+        /// <returns>Vista que da la opcion de cambiar de estado a un usuario</returns>
         public ActionResult State(Guid id)
         {
             User user = db.Users.Find(id);
@@ -166,13 +213,15 @@ namespace AplicacionBase.Controllers
             return View(user);
         }
 
-        //
-        // POST: /User/Delete/5
-
+        /// <summary>
+        /// Guarda los cambios realizados al estado de un usuario
+        /// </summary>
+        /// <param name="user">Usuario con toda su información</param>
+        /// <returns></returns>
         [HttpPost]
         public ActionResult State(User user)
         {
-            if (user != null)
+            if (ModelState.IsValid)
             {
                 db.Entry(user).State = EntityState.Modified;
                 db.SaveChanges();
@@ -183,17 +232,25 @@ namespace AplicacionBase.Controllers
             return View(user);
         }
 
+        /// <summary>
+        /// Método que carga la vista con el formulario para registrar un usuario en el sistema
+        /// </summary>
+        /// <returns>Vista que despliega el formulario que permite registrar los datos</returns>
         public ActionResult Register()
         {
             return View();
         }
 
+        /// <summary>
+        /// Guarda el usuario creado en el sistema
+        /// </summary>
+        /// <param name="model">Usuario a registrar</param>
+        /// <returns></returns>
         [HttpPost]
         public ActionResult Register(RegisterModel model)
         {
             if (ModelState.IsValid)
             {
-                // Intento de registrar al usuario
                 Guid g = System.Guid.Empty;
                 MembershipCreateStatus createStatus;
                 Membership.CreateUser(model.UserName, model.Password, model.Email, null, null, true, null, out createStatus);
@@ -208,10 +265,13 @@ namespace AplicacionBase.Controllers
                 TempData["info"] = "Si desea complete la Información del Usuario";
                 return RedirectToAction("Generate", "User", new { id = g });
             }
-            // Si llegamos a este punto, es que se ha producido un error y volvemos a mostrar el formulario
             return View(model);
         }
 
+        /// <summary>
+        /// Método que carga la vista con el formulario para crear la información personal de un usuario por parte del administrador
+        /// </summary>
+        /// <returns>Vista que despliega el formulario que permite crear los datos</returns>
         public ActionResult Generate(Guid id)
         {
             User user = db.Users.Find(id);
@@ -219,13 +279,16 @@ namespace AplicacionBase.Controllers
             return View(user);
         }
 
+        /// <summary>
+        /// Guarda los datos del usuario creado por parte del administrador
+        /// </summary>
+        /// <param name="user">Usuario con toda su información</param>
+        /// <returns></returns>
         [HttpPost]
         public ActionResult Generate(User user)
         {
-            //Guid h = System.Guid.Empty;
             if (ModelState.IsValid)
             {
-                //user.Id = h;
                 user.States = "true";
                 db.Users.Add(user);
                 db.SaveChanges();
@@ -236,6 +299,11 @@ namespace AplicacionBase.Controllers
             return View(user);
         }
 
+        /// <summary>
+        /// Método que permite salir de la vista principal de un usuario
+        /// </summary>
+        /// <param name="id">Id del usuario que ingresa al sistema</param>
+        /// <returns></returns>
         public ActionResult Out(Guid id)
         {
             Guid g = System.Guid.Empty;
@@ -260,6 +328,25 @@ namespace AplicacionBase.Controllers
             }
         }
 
+        /// <summary>
+        /// Método que permite buscar usuarios
+        /// </summary>
+        /// <param name="nameuser">Parametro de búsqueda</param>
+        /// <param name="page">Número de la página</param>
+        /// <returns></returns>
+        public ActionResult Search(string nameuser, int? page)
+        {
+            ViewBag.CurrentFilter = nameuser;
+            if (nameuser == null)
+            {
+                nameuser = " ";
+            }
+            var users = db.Users.Where(u => u.FirstNames.ToLower().Contains(nameuser) || u.LastNames.Contains(nameuser));
+            results = users.ToList().OrderByDescending(c => c.CellphoneNumber);
+            ViewBag.Busqueda = results;
+            pageNumber = (page ?? 1);
+            return View(results.ToPagedList(pageNumber, pageSize));
+        }
 
         protected override void Dispose(bool disposing)
         {
@@ -267,39 +354,5 @@ namespace AplicacionBase.Controllers
             base.Dispose(disposing);
         }
 
-        //
-        // GET: /User/Delete/5
-        /*
-                public ActionResult Delete(Guid id)
-                {
-                    User user = db.Users.Find(id);
-                    return View(user);
-                }
-
-                //
-                // POST: /User/Delete/5
-
-                [HttpPost, ActionName("Delete")]
-                public ActionResult DeleteConfirmed(Guid id)
-                {
-                    User user = db.Users.Find(id);
-                    //aspnet_Users w = db.aspnet_Users.Find(id);
-                    foreach (var ca in db.aspnet_Roles)
-                    {
-                        aspnet_UsersInRoles v = db.aspnet_UsersInRoles.Find(user.Id, ca.RoleId);
-                        if (v != null)
-                        {
-                            v.aspnet_Roles = null;
-                            v.aspnet_Users = null;
-                            db.aspnet_UsersInRoles.Remove(v);
-                        }
-                    }
-                    db.Users.Remove(user);
-                    //db.aspnet_Users.Remove(w);
-                    db.SaveChanges();
-                    TempData["Eliminado"] = "¡ Usuario Eliminado Correctamente !";
-                    return RedirectToAction("Index");
-                }
-        */
     }
 }
