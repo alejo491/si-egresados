6de87b5cb91d12b727bc7899ecba1be7bf9975c3
jmanuel0314@gmail.com <jmanuel0314@gmail.com@1a176d30-b54c-86d7-6163-4fd3ccc015e2>
2013-06-21 02:53:18 +0000

    Actualizacion de de las vistas Crear, Editar y Detalles Post

--- trunk/AplicacionBase/Controllers/PostController.cs
+++ trunk/AplicacionBase/Controllers/PostController.cs
@@ -62,13 +62,13 @@ namespace AplicacionBase.Controllers
         public ViewResult Details(Guid id)
         {
             Post post = db.Posts.Find(id);
-            Post post2 =new Post();
+            Post post2 = new Post();
             IList<Post> datos = new List<Post>();
             datos.Add(post);
 
             AplicacionBase.Controllers.LikeController lc = new LikeController();
             int cont = 0;
-            
+
             IList<Like> likes = lc.Index();
             //bool b= (Guid)Membership.GetUser().ProviderUserKey;
 
@@ -87,9 +87,9 @@ namespace AplicacionBase.Controllers
                 else post2.Autorized = 0;
             }
             else post2.Autorized = -1;
-            
 
-           
+
+
 
             datos.Add(post2);
             return View(datos);
@@ -103,7 +103,19 @@ namespace AplicacionBase.Controllers
         /// <returns>Retorna la vista para crear la noticia</returns>
         public ActionResult Create()
         {
-            return View();
+            Post post = new Post();
+            post.IdUser = (Guid)Membership.GetUser().ProviderUserKey;
+            post.PublicationDate = DateTime.Now;
+            post.Autorized = 0; //queda pendiente validar que rol tiene
+            post.Main = 0;
+            post.Estate = 1;
+            post.Id = Guid.NewGuid();
+            post.Title = "Remplace Por el Titulo de la Noticia";
+            post.Abstract = "Remplace Por el Resumen de la Noticia";
+            post.Content = "Remplace Por el Contenido de la Noticia";
+            db.Posts.Add(post);
+            db.SaveChanges();
+            return View(post);
         }
         #endregion
 
@@ -121,11 +133,10 @@ namespace AplicacionBase.Controllers
             post.Autorized = 0; //queda pendiente validar que rol tiene
             post.Main = 0;
             post.Estate = 1;
-
+            post.PublicationDate = DateTime.Now;
             if (ModelState.IsValid)
             {
-                post.Id = Guid.NewGuid();
-                db.Posts.Add(post);
+                db.Entry(post).State = EntityState.Modified;
                 db.SaveChanges();
                 return RedirectToAction("Index");
             }
@@ -142,9 +153,9 @@ namespace AplicacionBase.Controllers
         /// <returns>Retorna la noticia a editar</returns>
         public ActionResult Edit(Guid id)
         {
-           
+
             Post post = db.Posts.Find(id);
-             
+
             //ViewBag.IdUser = new SelectList(db.Users, "Id", "PhoneNumber", post.IdUser);
             return View(post);
         }
@@ -213,7 +224,7 @@ namespace AplicacionBase.Controllers
             return View(posts);
         }
 
-		public ActionResult ShowPost(Guid id)
+        public ActionResult ShowPost(Guid id)
         {
             //Post post = db.Posts.Find(id);
             //Guid us = (Guid)Membership.GetUser().ProviderUserKey;
@@ -232,7 +243,7 @@ namespace AplicacionBase.Controllers
 
             IList<Like> likes = lc.Index();
             int startbox = st.Index2(id);
-            
+
 
             if (Request.IsAuthenticated)
             {
@@ -245,7 +256,7 @@ namespace AplicacionBase.Controllers
                     }
                 }
                 if (cont == 1) post2.Autorized = 1;
-                else post2.Autorized= 0;
+                else post2.Autorized = 0;
 
             }
             else post2.Autorized = -1;
@@ -294,7 +305,7 @@ namespace AplicacionBase.Controllers
             {
                 db.Entry(post).State = EntityState.Modified;
                 db.SaveChanges();
-            }            
+            }
         }
 
         [HttpPost]
@@ -321,238 +332,5 @@ namespace AplicacionBase.Controllers
             }
         }
 
-///////////////////////////////////////////////////////////////////////////////////////////
-        [HttpPost]
-        public ActionResult Create2(AplicacionBase.Models.File uploadfile,Guid idpost)
-        {
-            if (ModelState.IsValid)
-            {
-                FilesPost filepost = new FilesPost();
-
-                filepost.IdPost = idpost;
-                if (uploadfile.Type.ToString().Contains("image"))
-                {
-                    filepost.Main = 1;
-                    filepost.Type = uploadfile.Type;
-                }
-                filepost.Main = 0;
-                filepost.Type = uploadfile.Type;
-                filepost.File = uploadfile;
-                db.FilesPosts.Add(filepost);
-                db.SaveChanges();
-            }
-
-            return View();
-        }
-
-        private string StorageRoot
-        {
-            get { return Path.Combine(Server.MapPath("~/UploadFiles")); }
-        }
-
-        //public ActionResult Index()
-        //{
-        //    return View();
-        //}
-
-
-        //DONT USE THIS IF YOU NEED TO ALLOW LARGE FILES UPLOADS
-        //[HttpGet]
-        //public void Delete(string id)
-        //{
-        //    var filename = id;
-
-        //    var filePath = Path.Combine(Server.MapPath("~/Files"), filename);
-
-        //    if (System.IO.File.Exists(filePath))
-        //    {
-        //        System.IO.File.Delete(filePath);
-        //    }
-        //}
-
-        //DONT USE THIS IF YOU NEED TO ALLOW LARGE FILES UPLOADS
-        [HttpGet]
-        public void Download(string id)
-        {
-            var filename = id;
-            var filePath = Path.Combine(Server.MapPath("~/UploadFiles"), filename);
-
-            var context = HttpContext;
-
-            if (System.IO.File.Exists(filePath))
-            {
-                context.Response.AddHeader("Content-Disposition", "attachment; filename=\"" + filename + "\"");
-                context.Response.ContentType = "application/octet-stream";
-                context.Response.ClearContent();
-                context.Response.WriteFile(filePath);
-            }
-            else
-                context.Response.StatusCode = 404;
-        }
-
-        //DONT USE THIS IF YOU NEED TO ALLOW LARGE FILES UPLOADS
-        //[HttpPost]
-        //public ActionResult UploadFiles2(FormCollection form)
-        //{
-
-        //}
-      
-        
-        [HttpPost]
-        public ActionResult UploadFiles(Post post)
-        {
-            var r = new List<ViewDataUploadFilesResult>();
-            Post post_temp = new Post();
-
-           
-            var IdUser = (Guid)Membership.GetUser().ProviderUserKey;
-
-            
-            post_temp.Title = post.Title;
-            post_temp.Abstract = post.Abstract;
-            post_temp.Content = post.Content;
-            post_temp.Autorized = 0; //queda pendiente validar que rol tiene
-            post_temp.Main = 0;
-            post_temp.Estate = 1;
-
-            Guid id_post = System.Guid.Empty;
-
-            foreach (var p in db.Posts)
-            {
-                if (p.Title.Equals(post_temp.Title) && p.Abstract.Equals(post_temp.Abstract))
-                {
-                    id_post = p.Id;
-                }
-            }
-
-            if (id_post == System.Guid.Empty)
-            {
-                post_temp.Id = Guid.NewGuid();
-                id_post = post_temp.Id;
-                post_temp.IdUser = IdUser;
-                post_temp.PublicationDate = DateTime.Now;
-                db.Posts.Add(post_temp);
-                db.SaveChanges();
-            }
-
-            foreach (string file in Request.Files)
-            {
-                var statuses = new List<ViewDataUploadFilesResult>();
-                var headers = Request.Headers;
-
-                if (string.IsNullOrEmpty(headers["X-File-Name"]))
-                {
-                    UploadWholeFile(Request, statuses);
-                }
-                else
-                {
-                    UploadPartialFile(headers["X-File-Name"], Request, statuses);
-                }
-                ViewDataUploadFilesResult obj_file = new ViewDataUploadFilesResult();
-
-                for (int i = 0; i < statuses.Count; i++)
-                {
-                    obj_file = statuses[i];//    if (ModelState.IsValid)
-                    {
-                        AplicacionBase.Models.File uploadfile = new AplicacionBase.Models.File();
-                        uploadfile.Id = Guid.NewGuid();
-                        uploadfile.Name = obj_file.name;
-                        uploadfile.Path = "/UploadFiles/"+obj_file.name.ToString();
-                        uploadfile.Type = obj_file.type;
-                        uploadfile.Size = obj_file.size.ToString();
-                        db.Files.Add(uploadfile);
-                        db.SaveChanges();
-                        Create2(uploadfile, id_post);
-                        
-                    }
-                }
-
-                JsonResult result = Json(statuses);
-                result.ContentType = "text/plain";
-                return result;
-            }
-
-            return Json(r);
-        }
-
-        private string EncodeFile(string fileName)
-        {
-            return Convert.ToBase64String(System.IO.File.ReadAllBytes(fileName));
-        }
-
-        //DONT USE THIS IF YOU NEED TO ALLOW LARGE FILES UPLOADS
-        //Credit to i-e-b and his ASP.Net uploader for the bulk of the upload helper methods - https://github.com/i-e-b/jQueryFileUpload.Net
-        private void UploadPartialFile(string fileName, HttpRequestBase request, List<ViewDataUploadFilesResult> statuses)
-        {
-            if (request.Files.Count != 1) throw new HttpRequestValidationException("Attempt to upload chunked file containing more than one fragment per request");
-            var file = request.Files[0];
-            var inputStream = file.InputStream;
-            var fullName = Path.Combine(StorageRoot,  Path.GetFileName(fileName));
-
-            using (var fs = new FileStream(fullName, FileMode.Append, FileAccess.Write))
-            {
-                var buffer = new byte[1024];
-
-                var l = inputStream.Read(buffer, 0, 1024);
-                while (l > 0)
-                {
-                    fs.Write(buffer, 0, l);
-                    l = inputStream.Read(buffer, 0, 1024);
-                }
-
-                fs.Flush();
-                fs.Close();
-            }
-
-            statuses.Add(new ViewDataUploadFilesResult()
-            {
-                name = fileName,
-                size = file.ContentLength,
-                type = file.ContentType,
-                url = "/Post/Download/" + fileName,
-                url_fullName = fullName,
-                delete_url = "/Post/Delete/" + fileName,
-                thumbnail_url = @"data:image/png;base64," + EncodeFile(fullName),
-                delete_type = "GET",
-            });
-        }
-
-        //DONT USE THIS IF YOU NEED TO ALLOW LARGE FILES UPLOADS
-        //Credit to i-e-b and his ASP.Net uploader for the bulk of the upload helper methods - https://github.com/i-e-b/jQueryFileUpload.Net
-        private void UploadWholeFile(HttpRequestBase request, List<ViewDataUploadFilesResult> statuses)
-        {
-            for (int i = 0; i < request.Files.Count; i++)
-            {
-                var file = request.Files[i];
-                var fullPath = Path.Combine(StorageRoot,Path.GetFileName(file.FileName));
-
-                file.SaveAs(fullPath);
-
-
-                statuses.Add(new ViewDataUploadFilesResult()
-                {
-                    name = file.FileName,
-                    size = file.ContentLength,
-                    type = file.ContentType,
-                    url = "/Post/Download/" + file.FileName,
-                    delete_url = "/Post/Delete/" + file.FileName,
-                    url_fullName = fullPath,
-                    thumbnail_url = @"data:image/png;base64," + EncodeFile(fullPath),
-                    delete_type = "GET",
-                });
-            }
-        }
-    }
-
-    public class ViewDataUploadFilesResult
-    {
-        public string name { get; set; }
-        public int size { get; set; }
-        public string type { get; set; }
-        public string url { get; set; }
-        public string delete_url { get; set; }
-        public string thumbnail_url { get; set; }
-        public string delete_type { get; set; }
-        public string url_fullName { get; set; }
     }
 }
