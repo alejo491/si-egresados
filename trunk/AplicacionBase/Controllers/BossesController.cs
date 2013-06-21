40407ce1a6de9aaeea5b9e76556820b1fa6bf532
crisarte22@gmail.com <crisarte22@gmail.com@1a176d30-b54c-86d7-6163-4fd3ccc015e2>
2013-06-21 17:06:46 +0000

    Documentacion agregada a ExperiencesController BossesController

--- trunk/AplicacionBase/Controllers/BossesController.cs
+++ trunk/AplicacionBase/Controllers/BossesController.cs
@@ -16,23 +16,34 @@ namespace AplicacionBase.Controllers
     {
         private DbSIEPISContext db = new DbSIEPISContext();
 
+
+        /// <summary>
+        /// Renderiza la pagina principal de vacantes
+        /// </summary>
+        /// <param name="wizardStep">Indicador de a que parte del wizard hace referencia esta funcion</param>
+        /// <returns>La vista con la página principal de jefes</returns>        
         public ActionResult Index()
         {
             return View(db.Bosses.ToList());
         }
 
-        //
-        // GET: /Bosses/Details/5
-
+        /// <summary>
+        /// Muestra los detalles para un jefe en especial
+        /// </summary>
+        /// <param name="id">Contiene el id del jefe de la cual se desean los detalles</param>
+        /// <returns>La vista con la jefe en detalle</returns>
         public ActionResult Details(Guid id)
         {
             Boss boss = db.Bosses.Find(id);
             return View(boss);
         }
 
-        //obtener detalles del jefe desde la vista de experiences
-        // GET: /Bosses/Details/5
-
+        /// <summary>
+        /// Muestra los detalles para un jefe en especial teniendo en cuenta el wizard
+        /// </summary>
+        /// <param name="id">Contiene el id del jefe de la cual se desean los detalles</param>
+        /// <param name="wizardStep">Indicador de a que parte del wizard hace referencia esta funcion</param>
+        /// <returns>La vista con la jefe en detalle</returns>
         public ActionResult DetailsForExperiences(Guid id, int wizardStep=0)
         {
             ViewBag.wizardStep = wizardStep;
@@ -41,18 +52,21 @@ namespace AplicacionBase.Controllers
         }
 
 
-        //
-        // GET: /Bosses/Create
-
+        /// <summary>
+        /// Atiende el resultado de pulsar el boton de Crear Nueva jefe en la vista principal
+        /// </summary>
+        /// <param name="wizardStep">Indicador de a que parte del wizard hace referencia esta funcion</param>
+        /// <returns>La vista de Creacion </returns>
         public ActionResult Create()
-        {
-           
+        {           
             return View();
         }
 
-        //
-        // POST: /Bosses/Create
-
+        /// <summary>
+        /// Atiende el resultado de hacer clic en el boton de Crear Nueva desde la vista de Creacion de jefes
+        /// </summary>
+        /// <param name="boss">Contiene los datos del jefe para ser llevados a la base de datos</param>
+        /// <returns>La vista al listado de jefes</returns>
         [HttpPost]
         public ActionResult Create(Boss boss)
         {
@@ -67,6 +81,11 @@ namespace AplicacionBase.Controllers
             return View();
         }
 
+        /// <summary>
+        /// Atiende el resultado de pulsar el boton de Crear Nuevo jefe en la vista principal
+        /// </summary>
+        /// <param name="wizardStep">Indicador de a que parte del wizard hace referencia esta funcion</param>
+        /// <returns>La vista de Creacion </returns>
         public ActionResult CreateForExperienceBosses(int wizardStep = 0)
         {
             ViewBag.wizardStep = wizardStep;
@@ -75,7 +94,11 @@ namespace AplicacionBase.Controllers
 
         //
         // POST: /Companies/CreateForExperience
-
+        /// <summary>
+        /// Atiende el resultado de hacer clic en el boton de Crear Nueva desde la vista de Creacion de jefes
+        /// </summary>
+        /// <param name="boss">Contiene los datos del jefe para ser llevados a la base de datos</param>
+        /// <returns>La vista al listado de jefes</returns>
         [HttpPost]
         public ActionResult CreateForExperienceBosses(Boss boss)
         {
@@ -92,18 +115,23 @@ namespace AplicacionBase.Controllers
         }
 
 
-        //
-        // GET: /Bosses/Edit/5
-
+        /// <summary>
+        /// Atiende el resultado de hacer clic en Editar, en las opciones de cada jefe
+        /// </summary>
+        /// <param name="boss">Contiene el id del jefe que se desea modificar</param>
+        /// <param name="wizardStep">Indicador de a que parte del wizard hace referencia esta funcion</param>
+        /// <returns>La vista con los datos a editar de la jefe</returns>
         public ActionResult Edit(Guid id)
         {
             Boss boss = db.Bosses.Find(id);
             return View(boss);
         }
 
-        //
-        // POST: /Bosses/Edit/5
-
+        /// <summary>
+        /// Atiende el resultado de hacer clic en Guardar Cambios de la vista de Edicion de jefes
+        /// </summary>
+        /// <param name="boss">Contiene los datos del jefe a actualizar</param>
+        /// <returns>La vista con el listado de jefes</returns>
         [HttpPost]
         public ActionResult Edit(Boss boss)
         {
@@ -116,18 +144,24 @@ namespace AplicacionBase.Controllers
             return View(boss);
         }
 
-        //
-        // GET: /Bosses/Delete/5
-
+        /// <summary>
+        /// Atiende el resultado de hacer clic en Eliminar, en las opciones de cada jefe
+        /// </summary>
+        /// <param name="id">Contiene el id del jefe que se desea Eliminar</param>
+        /// <param name="wizardStep">Indicador de a que parte del wizard hace referencia esta funcion</param>
+        /// <returns>La vista de confirmación</returns>
         public ActionResult Delete(Guid id)
         {
             Boss boss = db.Bosses.Find(id);
             return View(boss);
         }
 
-        //
-        // POST: /Bosses/Delete/5
-
+        /// <summary>
+        ///Atiende el resultado de hacer clic en Eliminar de la vista de Confirmacion de eliminacion de jefes
+        /// </summary>
+        /// <param name="id">Id de la jefe que se confirma se desea eliminar</param>
+        /// <param name="wizardStep">Indicador de a que parte del wizard hace referencia esta funcion</param>
+        /// <returns>La vista con el listado de jefes, con el jefe confirmado ya eliminada</returns>
         [HttpPost, ActionName("Delete")]
         public ActionResult DeleteConfirmed(Guid id)
         {
@@ -137,6 +171,10 @@ namespace AplicacionBase.Controllers
             return RedirectToAction("Index");
         }
 
+
+        /// <summary>
+        ///Dispose
+        /// </summary>
         protected override void Dispose(bool disposing)
         {
             db.Dispose();
