f8fdcdaba7515419d7558fee344acd2fe39e5876
pisunicauca@gmail.com <pisunicauca@gmail.com@1a176d30-b54c-86d7-6163-4fd3ccc015e2>
2013-06-22 21:56:31 +0000

    Terminado los todo el modulo de experiencias, integrado al wizard y trabajando cuando no esta en el wizard ...

--- trunk/AplicacionBase/Controllers/CompaniesController.cs
+++ trunk/AplicacionBase/Controllers/CompaniesController.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Web;
 using System.Web.Mvc;
 using AplicacionBase.Models;
+using System.Web.Routing;
 
 namespace AplicacionBase.Controllers
 {
@@ -71,15 +72,24 @@ namespace AplicacionBase.Controllers
         // POST: /Companies/CreateForExperience
 
         [HttpPost]
-        public ActionResult CreateForExperience(Company company)
+        public ActionResult CreateForExperience(Company company, FormCollection form)
         {
             if (ModelState.IsValid)
             {
+                int wizard=0;
+                 foreach(String key in form){
+                if (key.Contains("wizard"))
+                {
+                    if (form[key].ToString() == "1") { wizard = 1; }
+                }
+                }
+                
                 company.Id = Guid.NewGuid();
                 db.Companies.Add(company);
                 db.SaveChanges();
                 TempData["Create"] = "Se ha ingresado correctamente la compañía!";
-                return RedirectPermanent("/Experiences/create?wizardStep=1");
+                return RedirectToAction("Create", new RouteValueDictionary(new { controller = "Experiences", action = "Create", wizardStep = wizard }));
+                //return RedirectPermanent("/Experiences/create?wizardStep=1");
             }
 
             return View(company);
