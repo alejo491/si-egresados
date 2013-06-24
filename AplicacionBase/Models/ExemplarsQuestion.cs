a7e86c2f0b3e25b007c966d08605b05e99eaba4d
andresfeliperuizv@gmail.com <andresfeliperuizv@gmail.com@1a176d30-b54c-86d7-6163-4fd3ccc015e2>
2013-06-24 15:22:51 +0000

    Documentacion de los modelos

--- AplicacionBase/Models/ExemplarsQuestion.cs
+++ AplicacionBase/Models/ExemplarsQuestion.cs
@@ -3,17 +3,43 @@ using System.Collections.Generic;
 
 namespace AplicacionBase.Models
 {
+    /// <summary>
+    /// Representa la relacion entre los ejemplares y las preguntas
+    /// </summary>
     public partial class ExemplarsQuestion
     {
+        /// <summary>
+        /// Constructor de la clase
+        /// </summary>
         public ExemplarsQuestion()
         {
             this.Answers = new List<Answer>();
         }
 
+        /// <summary>
+        /// Codigo de la pregunta
+        /// </summary>
         public System.Guid IdQuestion { get; set; }
+        
+        /// <summary>
+        /// Codigo del ejemplar
+        /// </summary>
         public System.Guid IdExemplar { get; set; }
+
+        /// <summary>
+        /// Objeto de tipo respuesta
+        /// </summary>
         public virtual ICollection<Answer> Answers { get; set; }
+
+
+        /// <summary>
+        /// Objeto de tipo ejemplat
+        /// </summary>
         public virtual Exemplar Exemplar { get; set; }
+
+        /// <summary>
+        /// Objeto de tipo pregunta
+        /// </summary>
         public virtual Question Question { get; set; }
     }
 }
