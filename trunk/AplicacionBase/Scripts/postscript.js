55055a677526388c17f1efb550950fbbdba392b4
cesarzambrano91@gmail.com <cesarzambrano91@gmail.com@1a176d30-b54c-86d7-6163-4fd3ccc015e2>
2013-06-24 03:58:33 +0000

    mejora contenido de showpost

--- trunk/AplicacionBase/Scripts/postscript.js
+++ trunk/AplicacionBase/Scripts/postscript.js
@@ -40,3 +40,26 @@ function CambiarEstadoPost(postid) {
         data: { id: postid, value: value },        
     });
 }
+
+function DarFormato()
+{
+    var i=0;
+    var caracter;
+    var count;
+    var cadena = $("#texto-sin-formato").text();
+    var pi = 0;
+    for(i; i<cadena.length; i++){
+        caracter = cadena.charCodeAt(i);        
+        if(caracter == "13" || caracter == "10"){
+            if(cadena.charCodeAt(i+1) != "13" && cadena.charCodeAt(i+1) != "10"){
+                pi=i;
+            } 
+            if(cadena.charCodeAt(i-1) != "13" && cadena.charCodeAt(i-1) != "10"){
+                $("#texto-con-formato").append('<p>'+cadena.substring(pi,i)+'</p>');
+            } 
+        }        
+    }
+    if(cadena.charCodeAt(i) != "13" && cadena.charCodeAt(i) != "10"){        
+        $("#texto-con-formato").append('<p>'+cadena.substring(pi,i)+'</p>');
+    }
+}
