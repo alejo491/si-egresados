23056f8ba532f9ef16257a3dfe77625dd81c5061
ricardorado9@gmail.com <ricardorado9@gmail.com@1a176d30-b54c-86d7-6163-4fd3ccc015e2>
2013-06-24 21:57:54 +0000

    Documentación de modelos

--- AplicacionBase/Models/Company.cs
+++ AplicacionBase/Models/Company.cs
@@ -5,48 +5,81 @@ using System.ComponentModel.DataAnnotations;
 
 namespace AplicacionBase.Models
 {
+    /// <summary>
+    /// Clase que representa una compañía o empresa de la cual se requiere guardar alguna información
+    /// </summary>
     public partial class Company
     {
+        /// <summary>
+        /// Constructor de la clase
+        /// </summary>
         public Company()
         {
             this.Experiences = new List<Experience>();
             this.Vacancies = new List<Vacancy>();
         }
 
+        /// <summary>
+        /// Identificador único para cada compañía registrada
+        /// </summary>
         public System.Guid Id { get; set; }
 
-
+        /// <summary>
+        /// Nombre de la compañía o empresa
+        /// </summary>
         [Required(ErrorMessage = "Por favor ingrese un nombre para la compañía")]
+
         [Display(Name = "Nombre")]
         public string Name { get; set; }
 
+        /// <summary>
+        /// Teléfono de la compañía
+        /// </summary>
         [Required(ErrorMessage = "Por favor ingrese un teléfono para la compañía")]
         [Display(Name = "Teléfono")]
         [RegularExpression(@"[0-9]{7,10}", ErrorMessage = " No tiene el formato de Telefono")]
         public int Phone { get; set; }
 
+        /// <summary>
+        /// Dirección de alguna sede principal de la compañía
+        /// </summary>
         [Required(ErrorMessage = "Por favor ingrese la dirección de la compañía")]
         [Display(Name = "Dirección")]
         public string Address { get; set; }
 
+        /// <summary>
+        /// Correo electrónico para contactar con la compañía
+        /// </summary>        
         [Required(ErrorMessage = "Por favor ingrese un e-mail de la compañía")]
         [Display(Name = "E-Mail")]
         [RegularExpression(@"^[_a-z0-9-]+(\.[_a-z0-9-]+)*@[a-z0-9-]+(\.[a-z0-9-]+)*(\.[a-z]{2,3})$", ErrorMessage = " No tiene el formato de un correo electrónico")]
         public string Email { get; set; }
 
+        /// <summary>
+        /// Ciudad donde se encuentra la compañía, o alguna sede
+        /// </summary>
         [Required(ErrorMessage = "Por favor ingrese la ciudad donde se encuentra la compañía")]
         [Display(Name = "Ciudad")]
+        [RegularExpression(@"[A-Za-zñÑáéíóúÁÉÍÓÚ\s]*", ErrorMessage = "El formato es incorrecto")]
         public string City { get; set; }
 
-
+        /// <summary>
+        /// Sector empresarial donde se ubica la compañía
+        /// </summary>
         [Required(ErrorMessage = "Por favor ingrese el sector empresarial donde se encuentra la compañía")]
         [Display(Name = "Sector")]
+        [RegularExpression(@"[A-Za-zñÑáéíóúÁÉÍÓÚ\s]*", ErrorMessage = "El formato es incorrecto")]
         public string Sector { get; set; }
 
-
+        /// <summary>
+        /// Colección que hace referencia a personas que han trabajado en la compañía
+        /// </summary>
         [Display(Name = "Experiencias")]
         public virtual ICollection<Experience> Experiences { get; set; }
 
+        /// <summary>
+        /// Colección que hace referencia a vacantes u ofertas de empleo que se ofrecen en la compañia
+        /// </summary>
         [Display(Name = "Vacantes")]
         public virtual ICollection<Vacancy> Vacancies { get; set; }
     }
--- AplicacionBase/Models/ExperiencesBoss.cs
+++ AplicacionBase/Models/ExperiencesBoss.cs
@@ -4,26 +4,50 @@ using System.ComponentModel.DataAnnotations;
 
 namespace AplicacionBase.Models
 {
+    /// <summary>
+    /// Clase que guarda información sobre los jefes que estuvieron a cargo en una determinada experiencia laboral
+    /// </summary>
     public partial class ExperiencesBoss
     {
+        /// <summary>
+        /// Identificador de la clase
+        /// </summary>
         public System.Guid Id { get; set; }
+
+        /// <summary>
+        /// Identificador del jefe
+        /// </summary>
         public System.Guid IdBoss { get; set; }
+
+        /// <summary>
+        /// Identificador de la experiencia laboral
+        /// </summary>
         public System.Guid IdExperiences { get; set; }
 
+        /// <summary>
+        /// Fecha de inicio del mandato del jefe sobre el empleado
+        /// </summary>
         [Required(ErrorMessage = "Por favor ingrese la fecha de inicio")]
         [Display(Name = "Fecha de Inicio")]
         [DisplayFormat(ApplyFormatInEditMode = false, DataFormatString = "{0:D}")]
         public System.DateTime StartDate { get; set; }
 
+        /// <summary>
+        /// Fecha de finalización del mandato del jefe sobre el empleado
+        /// </summary>
         [Required(ErrorMessage = "Por favor ingrese la fecha de finalización")]
         [Display(Name = "Fecha de Finalización")]
         [DisplayFormat(ApplyFormatInEditMode = false, DataFormatString = "{0:D}")]
         public System.DateTime EndDate { get; set; }
 
-
+        /// <summary>
+        /// Objeto que referencia a un jefe
+        /// </summary>
         public virtual Boss Boss { get; set; }
 
-
+        /// <summary>
+        /// Objeto que referencia a una experiencia laboral
+        /// </summary>
         public virtual Experience Experience { get; set; }
     }
 }
