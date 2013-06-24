fcefb81c3328fb80345f860d84cc5cbd3f772cfc
jhonfredypaz <jhonfredypaz@1a176d30-b54c-86d7-6163-4fd3ccc015e2>
2013-06-24 17:15:33 +0000

    Documentación de modelos: Models/File.cs, Models/FilesPost.cs, Models/Like.cs, Models/Post.cs, Models/Startbox.cs y documentación del controlador FileController.cs

--- trunk/AplicacionBase/Models/Like.cs
+++ trunk/AplicacionBase/Models/Like.cs
@@ -3,13 +3,30 @@ using System.Collections.Generic;
 
 namespace AplicacionBase.Models
 {
+    /// <summary>
+    /// Representa la vista de la base de datos
+    /// </summary>
     public partial class Like
     {
+        /// <summary>
+        /// Identificador del like
+        /// </summary>
         public System.Guid Id { get; set; }
+        /// <summary>
+        /// Identificador del post
+        /// </summary>
         public System.Guid Id_Post { get; set; }
+        // <summary>
+        /// Identificador del usuario
+        /// </summary>
         public System.Guid Id_User { get; set; }
-
+        /// <summary>
+        /// Objeto de tipo Post
+        /// </summary>
         public virtual Post Post { get; set; }
+        /// <summary>
+        /// Objeto de tipo User
+        /// </summary>
         public virtual User User { get; set; }
     }
 }
--- trunk/AplicacionBase/Models/Startbox.cs
+++ trunk/AplicacionBase/Models/Startbox.cs
@@ -3,13 +3,35 @@ using System.Collections.Generic;
 
 namespace AplicacionBase.Models
 {
+    /// <summary>
+    /// Representa la vista de la base de datos
+    /// </summary>
     public partial class Startbox
     {
+        /// <summary>
+        /// Identificador del calificador
+        /// </summary>
         public System.Guid Id { get; set; }
+        /// <summary>
+        /// Identificador del post
+        /// </summary>
         public System.Guid Id_Post { get; set; }
+        /// <summary>
+        /// Identificador del usuario
+        /// </summary>
         public System.Guid Id_User { get; set; }
+        /// <summary>
+        /// Número de calificación
+        /// </summary>
         public int Qualification { get; set; }
+        /// <summary>
+        /// Objeto de tipo Post
+        /// </summary>
         public virtual Post Post { get; set; }
+        /// <summary>
+        /// Objeto de tipo User
+        /// </summary>
         public virtual User User { get; set; }
     }
 }
+
